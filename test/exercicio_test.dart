a04bb638603dc1636a80d92d9a7039794c3952ba
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-05-12 17:54:28 -0300

    Tests : Adding test to toBrazilianReal extension

--- test/exercicio_test.dart
+++ test/exercicio_test.dart
@@ -1,7 +1,12 @@
 import 'package:test/test.dart';
+import 'package:exercicio/extensions.dart';
 
 void main() {
-  test('test', () {
-    expect(true, true);
+  test('test toBrazilianReal extension whit negative value', () {
+    expect((-1.05).toBrazilianReal(), '-R\$ 1,05');
+  });
+
+  test('test toBrazilianReal extension whit positive value', () {
+    expect((1.05).toBrazilianReal(), 'R\$ 1,05');
   });
 }
